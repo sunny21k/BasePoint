4d0f6b723c6dad742e95755f0cf8194ec737649f
Sunny Kumar <sunny9102003kumar@gmail.com>
2026-04-02 22:30:00 -0400

    created user schema

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,20 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db";
+import cors from "cors";
 
 dotenv.config();
-
-connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
 
-app.get("/", (_req, res) => {
+app.use(cors());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send("API is running");
 });
 
